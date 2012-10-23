1bce76afd1652caaee3174557ffcf7aa747fb421
dlsstudios <dlsstudios@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-23 15:27:19 +0000

    Initial public version
    git-svn-id: https://plugins.svn.wordpress.org/sign-up-sheets/trunk@616338 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ export.php
@@ -0,0 +1,66 @@
+<?php
+/**
+* Export Sign-up Sheets to a File
+*/
+require_once $_SERVER['DOCUMENT_ROOT'].'/wp-load.php';
+
+if (!class_exists('DLS_SUS_Data')) require_once 'data.php';
+
+class DLS_SUS_Export
+{
+    
+    public $wpdb;
+    private $data;
+    
+    public function __construct()
+    {
+        global $wpdb;
+        $this->wpdb = $wpdb;
+        $this->data = new DLS_SUS_Data();
+    }
+    
+    /**
+     * Create export file with data
+     */
+    public function create()
+    {
+        $data = $this->data->get_all_data();
+        
+        header("Content-type: text/csv");
+        header("Content-Disposition: attachment; filename=sign-up-sheets-".date('Ymd-His').".csv");
+        header("Pragma: no-cache");
+        header("Expires: 0");
+        
+        $csv = '"Sheet ID","Sheet Title","Sheet Date","Task ID","Task Title","Sign-up ID","Sign-up Name","Sign-up Phone","Sign-up Email"'."\n";
+        foreach ($data as $d) {
+            $csv .= '"' . 
+                $this->clean_csv($d->sheet_id) . '","' . 
+                $this->clean_csv($d->sheet_title) . '","' . 
+                $this->clean_csv($d->sheet_date) . '","' . 
+                $this->clean_csv($d->task_id) . '","' . 
+                $this->clean_csv($d->task_title) . '","' . 
+                $this->clean_csv($d->signup_id) . '","' . 
+                $this->clean_csv($d->firstname) . '","' . 
+                $this->clean_csv($d->lastname) . '","' . 
+                $this->clean_csv($d->phone) . '","' . 
+                $this->clean_csv($d->email).'"'.
+            "\n";
+        }
+        echo $csv;
+    }
+    
+    /**
+     * Clean/escape CSV values
+     * 
+     * @param   string   input value
+     * @return  string   cleaned value
+     */
+    private function clean_csv($value)
+    {
+        return str_replace('"', '""', $value);
+    }
+    
+}
+$e = new DLS_SUS_Export();
+$e->create();
+?>
---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== Sign-up Sheet ===
+Contributors: DLS Software Studios
+Tags: volunteer, signup, sign up
+Requires at least: 3.0
+Tested up to: 3.4.1
+Stable tag: 1.0
+License: GPLv2 or later
+
+An online sign-up sheet manager where your users/volunteers can sign up for tasks
+
+== Description ==
+
+Sign-up Sheets allows you to have people sign up for different tasks and events that you set up.
+
+For more information, visit... [http://www.dlssoftwarestudios.com/sign-up-sheets-wordpress-plugin/]
+
+== Installation ==
+
+1. Download the plugin and extract the files
+2. Copy the `sign-up-sheets` directory and all its files to your `/wp-content/plugins/` directory
+3. Activate the plugin through the 'Plugins' menu in WordPress
+4. Create a new blank page and add the shortcode [sign_up_sheet]
+
+== Frequently Asked Questions ==
+
+= Can I change the "from" address on the confirmation email? =
+
+Yes, in `Settings > Sign-up Sheets` you can specify any email you want.  It defaults to the email address set in `Settings > General`.
+
+= How can I suggest an idea for the plugin? =
+
+Send us an email through our website at [http://www.dlssoftwarestudios.com/contact-us/]  We appreciate any and all feedback, but can't guarantee it will make it into the next version.  If you are in need a modification immediately, we are available for hire.  Please contact us at the link above and we can provide a quote.
+
+
+== Screenshots ==
+
+1. Admin Sign-up Sheets Listing
+2. Admin Sign-up Sheet Details
+3. Admin Edit Sign-up Sheet
+4. Frontend Example
+
+
+== Changelog ==
+
+= 1.0 =
+* Initial public version
